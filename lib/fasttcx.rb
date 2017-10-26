94f8dbf5f98971d95d30ec4f20e4d3fcf3564d72
Jonathan Simpson <jsimpson.github@gmail.com>
2017-10-25 22:36:51 -0700

    use structs classes

--- lib/fasttcx.rb
+++ lib/fasttcx.rb
@@ -1,102 +1,15 @@
-require 'ffi'
-
-class Coordinates < FFI::Struct
-  layout \
-    :latitude, :double,
-    :longitude, :double
-end
-
-class Trackpoint < FFI::Struct
-  layout \
-    :time, :pointer,
-    :latitude, :double,
-    :longitude, :double,
-    :elevation, :double,
-    :distance, :double,
-    :heart_rate, :int,
-    :cadence, :int,
-    :speed, :double,
-    :power, :int,
-    :next, Trackpoint.ptr
-end
-
-class Track < FFI::Struct
-  layout \
-    :num_trackpoints, :int,
-    :trackpoints, Trackpoint.ptr,
-    :next, Track.ptr
-end
-
-class Lap < FFI::Struct
-  layout \
-    :num_tracks, :int,
-    :num_trackpoints, :int,
-    :start_time, :pointer,
-    :total_time, :double,
-    :distance, :double,
-    :calories, :int,
-    :speed_average, :double,
-    :speed_maximum, :double,
-    :speed_minimum, :double,
-    :heart_rate_average, :int,
-    :heart_rate_maximum, :int,
-    :heart_rate_minimum, :int,
-    :intensity, :pointer,
-    :cadence_average, :int,
-    :cadence_maximum, :int,
-    :cadence_minimum, :int,
-    :elevation_maximum, :double,
-    :elevation_minimum, :double,
-    :total_elevation_gain, :double,
-    :total_elevation_loss, :double,
-    :tracks, Track.ptr,
-    :next, Lap.ptr
-end
-
-class Activity < FFI::Struct
-    layout \
-      :num_trackpoints, :int,
-      :num_laps, :int,
-      :started_at, :pointer,
-      :ended_at, :pointer,
-      :total_time, :double,
-      :start_point, Coordinates.ptr,
-      :end_point, Coordinates.ptr,
-      :latitude, :double,
-      :longitude, :double,
-      :total_calories, :int,
-      :total_distance, :double,
-      :total_elevation_gain, :double,
-      :total_elevation_loss, :double,
-      :speed_average, :double,
-      :speed_maximum, :double,
-      :speed_minimum, :double,
-      :elevation_maximum, :double,
-      :elevation_minimum, :double,
-      :cadence_average, :int,
-      :cadence_maximum, :int,
-      :cadence_minimum, :int,
-      :heart_rate_average, :int,
-      :heart_rate_minimum, :int,
-      :heart_rate_maximum, :int,
-      :laps, Lap.ptr,
-      :next, Activity.ptr
-end
-
-class TCX < FFI::Struct
-  layout \
-    :activities, Activity.ptr
-end
+require "ffi"
+require "fasttcx/structs/tcx"
 
 module FastTCX
   class C
     extend FFI::Library
 
-    ffi_lib "./libfasttcx.so"
+    ffi_lib "bin/libfasttcx.so"
 
     functions = [
-      [:parse_tcx_file, [TCX.by_ref, :pointer], :int],
-      [:calculate_summary, [TCX.by_ref], :void],
+      [:parse_tcx_file, [FastTCX::Structs::TCX.by_ref, :pointer], :int],
+      [:calculate_summary, [FastTCX::Structs::TCX.by_ref], :void],
     ]
 
     functions.each do |function|
@@ -110,7 +23,7 @@ module FastTCX
 
   class Ruby
     def initialize(file)
-      @tcx = TCX.new
+      @tcx = FastTCX::Structs::TCX.new
       @file = file
     end
 
@@ -132,14 +45,20 @@ module FastTCX
 
     private
 
+    def activities
+      @activities ||= @tcx[:activities]
+    end
+
     def laps
-      @laps ||= @tcx[:activities][:laps]
+      @laps ||= activities[:laps]
+    end
+
+    def tracks
+      @tracks ||= laps[:tracks]
+    end
+
+    def trackpoints
+      @trackpoints ||= tracks[:trackpoints]
     end
   end
 end
-
-fasttcx = FastTCX::Ruby.new("/Users/jsimpson/projects/github/js/fasttcx-rb/test.tcx")
-fasttcx.parse
-fasttcx.process
-fasttcx.print_laps
-
