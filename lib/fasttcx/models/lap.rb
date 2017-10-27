9dc109b5f16c138f100d3883226008d439df920f
Jonathan Simpson <jsimpson.github@gmail.com>
2017-10-26 20:53:05 -0700

    fix typos...

--- lib/fasttcx/models/lap.rb
+++ lib/fasttcx/models/lap.rb
@@ -3,12 +3,12 @@ require "ffi"
 module FastTCX
   module Models
     class Lap
-      attr_accessor :num_tracks, :num_tackpoints, :intensity,
+      attr_accessor :num_tracks, :num_trackpoints, :intensity,
         :start_time, :total_time, :distance, :calories,
         :speed_average, :speed_maximum, :speed_minimum,
         :heart_rate_average, :heart_rate_maximum, :heart_rate_minimum,
         :cadence_average, :cadence_maximum, :cadence_minimum,
-        :elevation_maximum, :elevation_maximum, :total_elevation_gain, :total_elevation_loss,
+        :elevation_maximum, :elevation_minimum, :total_elevation_gain, :total_elevation_loss,
 
       class << self
         def from_struct(struct)
